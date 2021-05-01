f5a2a85feb77a73787efc3f5b599555fc85baeb6
cat <nikhil.s.date@gmail.com>
2021-05-01 16:03:56 +0530

    added experimental coverage term

--- diagram_parser/circle_detector.py
+++ diagram_parser/circle_detector.py
@@ -35,7 +35,6 @@ def get_best_params(image, objective_function):
                                        param1=50, param2=param2, minRadius=min_radius,
                                        maxRadius=int(image.shape[0] / 2))
             if circles is not None:
-                circles = circles.astype(np.uint8)
                 num_circles = circles.shape[1]
 
                 trial_results.append((param2, min_radius, num_circles))
@@ -51,19 +50,35 @@ def get_best_params(image, objective_function):
                 x.append(param2)
                 y.append(min_radius)
                 circle_counts.append(0)
-    comparator = lambda trial: objective_function(*trial, max_circles, 100, int(image.shape[0] / 2))
+    comparator = lambda trial: objective_function(*trial, 100, int(image.shape[0] / 2), max_circles)
     sorted_results = sorted(trial_results, key=comparator)
     params = sorted_results[-1]
     return params
 
 
-def objective_function(param2, min_radius, num_circles, max_num_circles, max_param_2, max_radius):
+def area_of_circles(circles, image_shape):
+    # the list of circles is nested in another list to follow the output format of cv2.HoughCircles(_
+    pixels_inside = 0
+    for x in range(0, image_shape[0], 5):
+        for y in range(0, image_shape[1], 5):
+            inside = False
+            for circle in circles[0]:
+                if (x - circle[0]) ** 2 + (y - circle[1]) ** 2 <= circle[2] ** 2:
+                    inside = True
+                    break
+            if inside:
+                pixels_inside = pixels_inside + 1
+    return pixels_inside * 25 / (image_shape[0] * image_shape[1])
+
+
+def objective_function(param2, min_radius, num_circles, max_param_2, max_radius, max_circles):
     if num_circles == 0:
         return 0
     else:
         param2_term = 1 - math.exp(-5 * (param2 / max_param_2))
         min_radius_term = math.exp((-(min_radius / max_radius) ** 2))
         min_radius_term = 1 - (1.25 * (min_radius / max_radius - 0.2) ** 2)
+        num_circles_term = 1 - math.exp(-5*math.pow(num_circles/max_circles, 1/4))
         return param2_term + min_radius_term
 
 
@@ -75,9 +90,11 @@ def show_circles(image, circles):
         cv2.circle(color, (i[0], i[1]), 2, (0, 0, 255), 3)
     cv2.imshow('detected circles', color)
     cv2.waitKey()
+
+
 def clustering_filter(circles):
     circle_centers = [(circle[0], circle[1]) for circle in circles[0]]
-    clustering = DBSCAN(eps=0.05 * (img.shape[0] + img.shape[1]), min_samples=2).fit(circle_centers)
+    clustering = DBSCAN(eps=0.05 * (img.shape[0] + img.shape[1]), min_samples=1).fit(circle_centers)
     clusters = dict()
     filtered_circles = []
     for idx, label in enumerate(clustering.labels_):
@@ -91,22 +108,35 @@ def clustering_filter(circles):
 
     return np.array([filtered_circles])
 
-img = cv2.imread('NCERT-Solutions-for-Class-9-Maths-Chapter-10-Circles-Ex-10.4-Q4.1.png')
-img = preprocess(img)
-best_params = get_best_params(img, objective_function=objective_function)
-best_circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT,
-                                1.5, 1, param1=50, param2=best_params[0],
-                                minRadius=best_params[1], maxRadius=int(img.shape[0] / 2))
-best_circles = best_circles.astype(np.uint8)
-filtered_circles = clustering_filter(best_circles)
-print(best_params)
-cv2.imshow('preprocessed', img)
-show_circles(img, filtered_circles.astype(np.uint16))
+
+def detect_circles(image):
+    image = preprocess(image)
+    best_params = get_best_params(image, objective_function=objective_function)
+    # TODO: MAYBE USE A MORE SOPHISTICATED WAY OF DETERMINING IF THE IMAGE CONTAINS CIRCLES
+    if best_params[0] > 10:
+
+        best_circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT,
+                                        1.5, 1, param1=50, param2=best_params[0],
+                                        minRadius=best_params[1], maxRadius=int(image.shape[0] / 2))
+        best_circles = best_circles.astype(np.uint8)
+        filtered_circles = clustering_filter(best_circles)
+        print(best_params)
+        cv2.imshow('preprocessed', image)
+        show_circles(image, filtered_circles.astype(np.uint16))
+    else:
+        return []
+
+
+img = cv2.imread('circles.png')
+detect_circles(img)
+
+
 # ax = fig.gca(projection='3d')
 # ax.plot_trisurf(np.array(x), np.array(y), np.array(circle_counts), linewidth=0, antialiased=True)
 # plt.xlabel('param2')
 # plt.ylabel('min_radius')
 # plt.show()
+
 
 def filter_circles(circles):
     circles = list(circles[0])
